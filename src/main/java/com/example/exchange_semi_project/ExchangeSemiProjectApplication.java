6b1474d72a8e6f35abe490b16132827ea105954c
LeeJaeIn <wodls1616@4valuesoft.com>
2023-03-10 14:16:04 +0900

    create exchange_semi_project

---
+++ src/main/java/com/example/exchange_semi_project/ExchangeSemiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.exchange_semi_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeSemiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeSemiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exchange_semi_project/ExchangeSemiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exchange_semi_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeSemiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
